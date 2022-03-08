24cd0205f1b9dc84df1bbae7afae1cbcd2b899f1
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-08 19:23:57 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
 let casualNumber = 0;
 
 const generateBtn = document.getElementById("generate");
-// console.log(generateBtn);
+
+let userNumber;
+
+let numberBox = document.getElementById("number-box");
 
 let clock;
 generateBtn.addEventListener("click", function(){
-    let time = 0
-    clock = setInterval(() => {
-        time++;
-        console.log(time);
-
-        if(time == 30){
-            clearInterval(clock);
-            
-            let userNumber = parseInt(prompt("Inserisci un nmero"))
-        }
-    }, 1000);
 
     for(i = 1; i <= 5; i = i + 1){
         const number = document.createElement("h1");
@@ -24,8 +16,30 @@ generateBtn.addEventListener("click", function(){
         console.log(casualNumber);
         number.append(casualNumber);
         console.log(casualNumber);
-        document.getElementById("number-box").append(number)
+        numberBox.append(number);
     }
+
+    let time = 0
+    clock = setInterval(() => {
+        time++;
+        document.querySelector("p").innerHTML = time;
+
+        if(time == 10){
+            clearInterval(clock);
+            numberBox.innerHTML = "";
+
+            for(let i = 0; i < 5; i = i + 1){
+                userNumber = parseInt(prompt("Inserisci un nmero"));
+                console.log(userNumber);
+            }
+
+            if(userNumber == casualNumber){
+                document.querySelector("p").innerHTML = "Hai Indovinato!"
+            }else{
+                document.querySelector("p").innerHTML = "Non hai Indovinato!"
+            }
+        }
+    }, 1000);
 })
 
 
